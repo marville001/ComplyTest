0e8b4ae3748117763a04470b33d2f3f7954b3cb7
Martin Mwangi <mwangimartin1904@gmail.com>
2025-07-31 12:18:50 +0300

    Propert url for random code generation

--- ComplyTest.Infrastructure/Services/RandomStringGeneratorService.cs
+++ ComplyTest.Infrastructure/Services/RandomStringGeneratorService.cs
@@ -1,10 +1,19 @@
 using System;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using ComplyTest.Domain.Interfaces;
 using Microsoft.Extensions.Configuration;
 
 namespace ComplyTest.Infrastructure.Services;
+
+public class CodeGeneratorRequest
+{
+    public int codesToGenerate { get; set; }
+    public bool onlyUniques { get; set; }
+    public string[] charactersSets { get; set; }
+}
 
 public class RandomStringGeneratorService : IRandomStringGeneratorService
 {
@@ -14,18 +23,32 @@ public class RandomStringGeneratorService : IRandomStringGeneratorService
     public RandomStringGeneratorService(HttpClient httpClient, IConfiguration configuration)
     {
         _httpClient = httpClient;
-        _apiUrl = configuration["RandomStringGeneratorApiUrl"] ?? "https://www.random.org/strings/?num=1&len=10&digits=on&upperalpha=on&loweralpha=on&unique=off&format=plain&rnd=new";
+        _apiUrl = configuration["RandomStringGeneratorApiUrl"] ?? "https://codito.io/free-random-code-generator/api/generate";
     }
 
     public async Task<string> GenerateRandomStringAsync()
     {
         try
         {
-            var response = await _httpClient.GetAsync(_apiUrl);
+
+            var requestData = new CodeGeneratorRequest
+            {
+                codesToGenerate = 1,
+                onlyUniques = false,
+                charactersSets = new string[] { "\\d\\l\\L\\@", "\\d\\l\\L\\@", "\\d\\l\\L\\@", "\\d\\l\\L\\@", "\\d\\l\\L\\@", "\\d\\l\\L\\@", "\\d\\l\\L\\@", "\\d\\l\\L\\@" }
+            };
+
+            var json = JsonSerializer.Serialize(requestData);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(_apiUrl, content);
             response.EnsureSuccessStatusCode();
 
             var randomString = await response.Content.ReadAsStringAsync();
-            return randomString.Trim();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var codes = JsonSerializer.Deserialize<string[]>(responseContent);
+            return codes?[0] ?? throw new Exception("Failed to generate random string");
         }
         catch
         {
